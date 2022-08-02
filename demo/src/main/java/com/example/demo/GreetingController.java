2e11abb51c5a2035d51c5ecde5811b9316359ad5
Corvettemina <mina.h.hanna@gmail.com>
2022-08-01 21:40:41 -0400

    update

--- demo/src/main/java/com/example/demo/GreetingController.java
+++ demo/src/main/java/com/example/demo/GreetingController.java
@@ -10,18 +10,35 @@ import api.CrDateTime;
 
 @RestController
 public class GreetingController {
-    private static final String template = setDate() + "  %s!";
+
+    static int[] test = { 2022, 10, 10 };
+    //private static final String template = setDate(test) + "  %s!";
     private final AtomicLong counter = new AtomicLong();
-    private String test = setDate();
+    private final static String nameTest = "testymctest";
+    // private String test = setDate();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = nameTest) String name) {
+        String test1 = "MINA";
+
+        // return new Greeting(counter.incrementAndGet(), String.format(test1, name));
+        String year = name.substring(0, 5);
+        String month = name.substring(4, 7);
+        String day = name.substring(6);
+        String test2 = setDate(year, day, month);
+        return new Greeting(counter.incrementAndGet(), (test1 + " " + test2));
     }
 
     public static String setDate() {
         Calendar cal = Calendar.getInstance();
-        //cal.set(2022, 4, 17);
+        // cal.set(2022, 4, 17);
+        CrDateTime cr = new CrDateTime(cal);
+        return cr.asCopticDate().toCopticString();
+    }
+
+    public static String setDate(String year, String month, String day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Integer.valueOf(year), Integer.valueOf(month), Integer.valueOf(day));
         CrDateTime cr = new CrDateTime(cal);
         return cr.asCopticDate().toCopticString();
     }
