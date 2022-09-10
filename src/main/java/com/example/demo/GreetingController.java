7803c0de4f387390eeee1047f69eeca850e66365
Corvettemina <mina.h.hanna@gmail.com>
2022-09-09 23:51:50 -0400

    update

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +22,6 @@ public class GreetingController {
 
     private final AtomicLong counter = new AtomicLong();
 
-   
     @CrossOrigin
     @GetMapping("/greeting")
     public ArrayList<Object> greeting(@RequestParam(value = "date", defaultValue = "default") String date)
