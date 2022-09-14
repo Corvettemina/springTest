6dff9ecefedcec0233c7eb41a56f941213e0714d
Corvettemina <mina.h.hanna@gmail.com>
2022-09-14 18:26:41 -0400

    update

---
+++ Python/mergepptxaspose.py
@@ -0,0 +1,49 @@
+import asposeslidescloud
+import shutil
+from asposeslidescloud.apis.slides_api import SlidesApi
+from asposeslidescloud.models import *
+import platform
+import springApiTest
+
+platform.platform()
+if("Windows" in platform.platform()):
+    path = "C:/Users/Mina Hanna/OneDrive/"
+if(("Linux" in platform.platform())):
+    path = "root/Dropbox/"
+
+slides_api = SlidesApi(None, "2d3b1ec8-738b-4467-915f-af02913aa7fa", "1047551018f0feaacf4296fa054d7d97")
+files = []
+for i in springApiTest.getlist():
+    with open(path + i, "rb") as file_stream:
+        files.append(file_stream.read())
+
+slides_api.merge_and_save_online("MyPresentation.pptx", files, None, "internal")
+
+storage_name = "internal"
+file_path = "MyPresentation.pptx"
+result_path = path + "result.pptx"
+
+temp_path = slides_api.download_file("MyPresentation.pptx", "internal")
+shutil.copyfile(temp_path, result_path)
+
+'''
+import springApiTest
+
+request = OrderedMergeRequest()
+lists = []
+for i in springApiTest.getlist():
+    presentation = PresentationToMerge()
+    presentation.path = i
+    presentation.source = "Storage"
+    lists.append(presentation)
+
+request.presentations = lists
+
+slides_api.merge_and_save_online("Powerpoints/blank.pptx", None,request,"Dropbox1")
+
+
+# Merge the presentations.
+result_file_path = slides_api.merge_online(files)
+
+print("The output presentation was saved to ", result_file_path)
+'''
---
+++ Python/springApiTest.py
@@ -0,0 +1,25 @@
+import requests
+import json
+def getlist():
+    response = requests.get('http://192.81.219.24:8080/greeting?date=2022-09-11')
+    answer = []
+    y = json.loads(response.text)
+    
+    #print(y[0]['standardDoxologies'])
+
+    for i in y[1]:
+        if(i != "Ocassion" and i != "Season" and i != "Sunday"):
+            if(type(y[1][i]) is list):
+                for l in y[1][i]:
+                    #print(l)
+                    answer.append(l)
+            else:
+                #print(y[1][i])
+                if(y[1][i] != ""):
+                    answer.append(y[1][i])
+    print(answer)
+    return answer
+import platform
+print(platform.platform())
+for i in getlist():
+    print(i)
